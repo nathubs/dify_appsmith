63020d11238d3b05e95810bbc0cac17671b42f38
tianhong.huang <tianhong.huang@ubtrobot.com>
2024-11-07 14:32:14 +0000

    添加应用详情页面

---
+++ pages/应用详情/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,39 @@
+export default {
+	SelectDateonDropdownClose () {
+		//	write code here
+		TotalConversationsByAppID.run()
+		total_active_user_num.run()
+		ActiveUsersByAppID.run()
+		total_active_user_num.run()
+		AvgSessionInteractionsByAppID.run()
+		total_avg_ssn_interaction_num.run()
+		TokenOutputSpeedByAppID.run()
+		total_avg_token_output_speed.run()
+		UserSatisfactionRateByAppID.run()
+		total_avg_user_stf_rate.run()
+		TokenUsageByAppID.run()
+		total_token_usage.run()
+		TotalMessagesByAppID.run()
+		total_message_num.run()
+	},
+	GetSeletData() {
+		return [
+  {
+    "name": "过去一周",
+    "code": "{{utils.getLastDaysRange(7)}}"
+  },
+  {
+    "name": "过去一个月",
+    "code": "{{utils.getLastDaysRange(30)}}"
+  },
+	{
+    "name": "过去一季度",
+    "code": "{{utils.getLastDaysRange(90)}}"
+  },
+	{
+    "name": "过去一年",
+    "code": "{{utils.getLastDaysRange(365)}}"
+  }
+	]
+	}
+}
---
+++ pages/应用详情/jsobjects/utils/utils.js
@@ -0,0 +1,73 @@
+export default {
+	// initialize: () => {
+		// storeValue('docIndex', 0);
+		// storeValue('selectedStatus', '')
+	// },
+	getAppID: ()=> {
+		return appsmith.URL.queryParams.selectedAppID;
+	},
+	getAppName: ()=> {
+		return appsmith.URL.queryParams.selectedAppName;
+	},
+	formatDate: (date)=> {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份需要加1
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+	},
+  getLastDaysRange: (dayNum)=> {
+    const today = new Date();
+    // const endDate = new Date(today); // 今天是结束日期
+    const startDate = new Date(today);
+    startDate.setDate(today.getDate() - dayNum); // 最近7天开始日期
+		return utils.formatDate(startDate);
+	},
+	getLastDays: (dayNum)=> {
+    const today = new Date();
+    // const endDate = new Date(today); // 今天是结束日期
+    const startDate = new Date(today);
+    startDate.setDate(today.getDate() - dayNum); // 最近7天开始日期
+		return startDate.valueOf()/1000
+	},
+	getQueryData: (query_func)=> {
+		var query_data = query_func.data;
+		console.log(query_data);
+		if (query_data !== '[]') {
+			return query_data;
+		}
+		query_data = [];
+		const today = new Date();
+		for (var i = 7; i > 0; i--) {
+			const startDate = new Date(today);
+			startDate.setDate(today.getDate() - i);
+			query_data.push({'x': startDate, 'y': 0})
+		}
+		return query_data;
+	},
+	transferNumUnit: (num)=> {
+		if (num > 1000) {
+			return Math.floor(num / 1000) + 'k';
+		}
+		else if (num > 1000 * 1000) {
+			return Math.floor(num / 1000 / 1000) + 'm';
+		}
+		else if (num > 1000 * 1000 * 1000) {
+			return Math.floor(num / 1000 / 1000 / 1000) + 'b';
+		}
+		else {
+			return num
+		}
+	},
+	// test: ()=> {
+		// let res = utils.str2timestamp(SelectDate.selectedOptionValue)
+		// console.log(res)
+		// return res
+	// },	
+	str2timestamp: (dateString)=> {
+		// let dateString = "2023-10-04"; // 自定义格式的日期字符串
+		let parts = dateString.split("-");
+		let date = new Date(parts[0], parts[1] - 1, parts[2]);
+		let timestamp = date.getTime();	
+		return timestamp/1000;
+	},	  
+}
