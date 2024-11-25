99ca2eab3b0488046b03d7e3dec741caad9c5311
tianhong.huang <tianhong.huang@ubtrobot.com>
2024-11-25 08:43:56 +0000

    修复增加workflow类型的bot后引发的相关BUG，并调整了消息数和活跃用户Tab页的布局，保持和消费额度一致

--- pages/应用报表/jsobjects/JSOApps/JSOApps.js
+++ pages/应用报表/jsobjects/JSOApps/JSOApps.js
@@ -3,8 +3,8 @@ export default {
 		storeValue('tokenChartType', "COLUMN_CHART");
 		storeValue('tabName','消费额度');
 	},  	
-	switchChartView: () => {
-		if (RadioGroup1.selectedOptionValue === "Y") {
+	switchChartView: (radio_group) => {
+		if (radio_group.selectedOptionValue === "Y") {
 			storeValue('tokenChartType', "COLUMN_CHART");
 		} else {
 			storeValue('tokenChartType', "PIE_CHART");
