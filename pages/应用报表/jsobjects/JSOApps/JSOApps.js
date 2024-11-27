7315bb2d7785d294f76c97da4e28eca4bb7ef118
tianhong.huang <tianhong.huang@ubtrobot.com>
2024-11-27 08:51:20 +0000

    修复应用报表页面切换不同Tab页，图表类型和实际显示图表不一致的问题，同时修复从应用报表页面切换到其他页面，再返回应用报表页面，Tab既没有重置到默认Tab，也没有显示正确图表类型的图表问题，当前的解决方案是切换页面后自动重置到默认Tab和默认图表类型

--- pages/应用报表/jsobjects/JSOApps/JSOApps.js
+++ pages/应用报表/jsobjects/JSOApps/JSOApps.js
@@ -2,8 +2,21 @@ export default {
 	initialize: () => {
 		storeValue('tokenChartType', "COLUMN_CHART");
 		storeValue('tabName','消费额度');
+		navigateTo('应用报表', {tab: '消费额度'})
 	},  	
-	switchChartView: (radio_group) => {
+	switchChartView: () => {
+		var radio_group = RadioGroup1;
+		switch(appsmith.store.tabName) {
+			case '消费额度':
+				radio_group = RadioGroup1;
+				break;
+			case '消息数':
+				radio_group = RadioGroup3;
+				break;
+			case '活跃用户':
+				radio_group = RadioGroup2;
+				break;
+		}
 		if (radio_group.selectedOptionValue === "Y") {
 			storeValue('tokenChartType', "COLUMN_CHART");
 		} else {
--- pages/应用报表/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,9 +0,0 @@
-export default {
-	
-	Tabs1onTabSelected () {
-		//	write code here
-		//console.log(appsmith.store.tabName)		
-		AppsQuery.run()		
-		//console.log(appsmith.store.tabName=='消息数'?"message_count":(appsmith.store.tabName=='消费额度'?"token_count":"terminal_count")) 
-	}
-}
