2506d126500cde7b86b9528097865f58f2edc3f6
tianhong.huang <tianhong.huang@ubtrobot.com>
2024-11-28 03:25:25 +0000

    新增功能，工作空间页面双击某个工作空间跳转到该空间所属的应用报表页面

--- pages/应用报表/jsobjects/JSOApps/JSOApps.js
+++ pages/应用报表/jsobjects/JSOApps/JSOApps.js
@@ -2,8 +2,39 @@ export default {
 	initialize: () => {
 		storeValue('tokenChartType', "COLUMN_CHART");
 		storeValue('tabName','消费额度');
+		storeValue('workspaceID', appsmith.URL.queryParams.workspaceID)
+		storeValue('workspaceName', appsmith.URL.queryParams.workspaceName)
 		navigateTo('应用报表', {tab: '消费额度'})
-	},  	
+		AppsQueryByPage.run()
+		TotalRecordAppQuery.run()
+	},
+	getWorkspaceID: () => {
+		var w_id = appsmith.store.workspaceID;
+		if (w_id) {
+			return w_id;
+		}
+		else {
+			return '00000000-0000-0000-0000-000000000000';
+		}
+	}, 
+	getWorkspaceName: () => {
+		var w_name = appsmith.store.workspaceName;
+		if (w_name) {
+			return w_name;
+		}
+		else {
+			return '';
+		}
+	}, 
+	isFromWorkspace: () => {
+		var w_id = appsmith.store.workspaceID;
+		if (w_id) {
+			return 'true';
+		}
+		else {
+			return 'false';
+		}
+	}, 
 	switchChartView: () => {
 		var radio_group = RadioGroup1;
 		switch(appsmith.store.tabName) {
